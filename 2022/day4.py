6f2c85f7224bba3a24f5a3d68b0ff235e7948a1c
Scott Yost <scott@yostage.com>
2022-12-04 15:10:35 -0800

    day4.1

---
+++ 2022/day4.py
@@ -0,0 +1,41 @@
+import functools
+import itertools
+from typing import Iterator, List
+
+
+def input_string_to_intervals(s: str):
+    # "2-4,6-8" => (2,4),(6,8)
+    pairs = s.split(",")
+    assert len(pairs) == 2
+    first = [int(i) for i in pairs[0].split("-")]
+    assert len(first) == 2
+    second = [int(i) for i in pairs[1].split("-")]
+    assert len(second) == 2
+    return [first, second]
+
+
+def compare_intervals(a, b) -> int:
+    # otherwise sort by first interval ascending
+    if a[0] != b[0]:
+        return a[0] - b[0]
+    else:  # tie break on length, descending
+        return (b[1] - b[0]) - (a[1] - a[0])
+
+
+def intervals_overlap(intervals) -> bool:
+    assert len(intervals) == 2
+    intervals.sort(key=functools.cmp_to_key(compare_intervals))
+    return intervals[0][0] <= intervals[1][0] and intervals[0][1] >= intervals[1][1]
+
+
+def main():
+    with open("day4_input.txt", "r") as file:
+        intervalList = [
+            input_string_to_intervals(line) for line in file.read().splitlines()
+        ]
+        matches = [1 if intervals_overlap(i) else 0 for i in intervalList]
+        print(sum(matches))
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2022/day4_tests.py
@@ -0,0 +1,52 @@
+import functools
+import itertools
+from typing import Iterator
+import unittest
+
+from day4 import input_string_to_intervals, compare_intervals, intervals_overlap
+
+
+class TestDay4(unittest.TestCase):
+
+    example = """2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8"""
+
+    def test_example1(self):
+        intervalList = [
+            input_string_to_intervals(line) for line in self.example.splitlines()
+        ]
+        matches = [1 if intervals_overlap(i) else 0 for i in intervalList]
+        self.assertEqual(sum(matches), 2)
+
+    def test_input_parsing(self):
+        l1 = self.example.splitlines()[0]
+        self.assertEqual(input_string_to_intervals(l1), [[2, 4], [6, 8]])
+
+    def test_compare_intervals(self):
+        self.assertEqual(compare_intervals([1, 2], [1, 2]), 0)
+        self.assertLess(compare_intervals([1, 2], [2, 3]), 0)
+        self.assertLess(compare_intervals([1, 3], [1, 2]), 0)
+
+    def test_sort_intervals(self):
+        self.assertListEqual(
+            sorted([[5, 6], [2, 3]], key=functools.cmp_to_key(compare_intervals)),
+            [[2, 3], [5, 6]],
+        )
+
+        self.assertListEqual(
+            sorted([[1, 2], [1, 3]], key=functools.cmp_to_key(compare_intervals)),
+            [[1, 3], [1, 2]],
+        )
+
+    def test_intervals_overlap(self):
+        self.assertTrue(intervals_overlap([[1, 2], [1, 3]]))
+        self.assertFalse(intervals_overlap([[1, 2], [2, 3]]))
+        self.assertFalse(intervals_overlap([[1, 3], [2, 4]]))
+
+
+if __name__ == "__main__":
+    unittest.main()
