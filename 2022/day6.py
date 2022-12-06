5a5d980e5c5e1a4cd6feb6817008032dbf72b4c5
Scott Yost <scott@yostage.com>
2022-12-05 21:25:11 -0800

    6.2

--- 2022/day6.py
+++ 2022/day6.py
@@ -1,11 +1,11 @@
-def find_signal(line: str):
+def find_signal(line: str, unique_len=4):
     # base case
     chars = list(line)
-    buffer = chars[0:4]
-    marker = 4
-    del chars[0:4]
+    buffer = chars[0:unique_len]
+    marker = unique_len
+    del chars[0:unique_len]
     for c in chars:
-        if len(set(buffer)) == 4:
+        if len(set(buffer)) == unique_len:
             return marker
         del buffer[0]
         buffer += c
@@ -18,7 +18,10 @@ def main():
     with open("day6_input.txt", "r") as file:
         lines = file.read().splitlines()
 
+    # part one
     print(find_signal(lines[0]))
+    # part two
+    print(find_signal(lines[0], 14))
 
 
 if __name__ == "__main__":
--- 2022/day6_tests.py
+++ 2022/day6_tests.py
@@ -1,52 +1,20 @@
 import functools
 import itertools
-from typing import Iterator
 import unittest
+from typing import Iterator
 
-from day5 import (
-    cratelines_to_crates,
-    parse_crateline,
-    parse_input,
-    parse_instructions,
-    part_one,
-    part_two,
-    # input_string_to_intervals,
-    # compare_intervals,
-    # intervals_completely_overlap,
-    # intervals_overlap,
-)
+from day6 import find_signal
 
 
 class TestDay6(unittest.TestCase):
 
     example1 = """bvwbjplbgvbhsrlpgdmjqwftvncz"""
 
-    def test_find_signal(self):
-        def find_signal(line: str):
-            # base case
-            chars = list(line)
-            buffer = chars[0:4]
-            marker = 4
-            del chars[0:4]
-            for c in chars:
-                if len(set(buffer)) == 4:
-                    return marker
-                del buffer[0]
-                buffer += c
-                marker += 1
-
-            assert False
-
+    def test_part_one(self):
         self.assertEqual(find_signal(self.example1), 5)
-        # shift
 
-    # def test_part_one(self):
-    #     answer = part_one(self.example.splitlines())
-    #     self.assertEqual(answer, ["C", "M", "Z"])
-
-    # def test_part_two(self):
-    #     answer = part_two(self.example.splitlines())
-    #     self.assertEqual(answer, ["M", "C", "D"])
+    def test_part_two(self):
+        self.assertEqual(find_signal(self.example1, 14), 23)
 
 
 if __name__ == "__main__":
