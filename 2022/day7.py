2e3bd3343af34429310c50bdcd2111018488b7d6
Scott Yost <scott@yostage.com>
2022-12-07 22:12:59 -0800

    i'm all done with you 7.2

--- 2022/day7.py
+++ 2022/day7.py
@@ -1,3 +1,4 @@
+from functools import cache
 import re
 from typing import Any, Dict, List
 
@@ -23,44 +24,89 @@ class ElvenDirectory:
     files: List[ElvenFile] = field(default_factory=list)
     directories: Dict[str, Any] = field(default_factory=dict)
 
+    # @cache
+    def get_recursive_size(self) -> int:
+        return sum([f.size for f in self.files]) + sum(
+            [d.get_recursive_size() for d in self.directories.values()]
+        )
 
-def parse_lines(lines: List[str]):
+    def dfs(self):
+        for d in self.directories.values():
+            yield from d.dfs()
+        yield self
+
+
+def parse_lines(lines: List[str]) -> ElvenDirectory:
     root = ElvenDirectory(name="/", parent=None)
     cwd = root
+
     for line in lines:
         if match := re.search(r"\$ cd (\w+)", line):
-            (filename) = match.groups()
-            print(f"CD into {filename}")
-            cwd = cwd.directories[str(filename)]
+            filename = str(match.groups()[0])
+            # print(f"CD into {filename}")
+            cwd = cwd.directories[filename]
         elif match := re.search(r"dir (\w+)", line):
-            (filename) = match.groups()
-            print(f"child dir {filename}")
-            cwd.directories[str(filename)] = ElvenDirectory(
-                name=str(filename), parent=cwd
-            )
+            filename = str(match.groups()[0])
+            # print(f"child dir {filename}")
+            assert cwd.directories.get(filename) is None
+            cwd.directories[filename] = ElvenDirectory(name=filename, parent=cwd)
+            # print(f"\tcwd {cwd.name} now has {len(cwd.directories.values())} children")
         elif line == "$ ls":
-            print("ls")
+            # print("ls")
+            continue
         elif line == "$ cd ..":
-            print("cd up")
+
             cwd = cwd.parent
+            # print(f"cd .. [cwd is now {cwd.name}]")
         elif line == "$ cd /":
-            print("cd root")
+            # print("cd root")
             cwd = root
         elif match := re.search(r"(\d+) ([\w\.])+", line):
             (size, name) = match.groups()
-            print(f"file [{name}] has size [{size}]")
+            # print(f"file [{name}] has size [{size}]")
             cwd.files.append(ElvenFile(name=name, size=int(size)))
         else:
             print(f"UNMATCHED LINE {line}")
             assert False
 
-    return None
+    return root
+
+
+def part_one(lines):
+    root = parse_lines(lines)
+    print(root.get_recursive_size())
+    # self.assertEqual(find_signal(self.example1), 5)
+
+    # for d in root.dfs():
+    #     print(f"directory {d.name} has size {d.get_recursive_size()}")
+    big_dirs = [d for d in root.dfs() if d.get_recursive_size() <= 100000]
+    return sum([d.get_recursive_size() for d in big_dirs])
+    # print(sum([d.get_recursive_size() for d in big_dirs]))
+
+
+def part_two(lines):
+    root = parse_lines(lines)
+    total = 70000000
+    need_available = 30000000
+    current_available = total - root.get_recursive_size()
+    need_to_find = need_available - current_available
+    # print(root.get_recursive_size())
+    # self.assertEqual(find_signal(self.example1), 5)
+
+    # for d in root.dfs():
+    #     print(f"directory {d.name} has size {d.get_recursive_size()}")
+    big_dirs = [d for d in root.dfs() if d.get_recursive_size() >= need_to_find]
+
+    # for d in big_dirs:
+    #     print(f"{d.name}: {d.get_recursive_size()}")
+    return min([d.get_recursive_size() for d in big_dirs])
+    # print(sum([d.get_recursive_size() for d in big_dirs]))
 
 
 def main():
     with open("day7_input.txt", "r") as file:
         lines = file.read().splitlines()
-        parse_lines(lines)
+        print(part_two(lines))
 
     # part one
     # print(find_signal(lines[0]))
--- 2022/day7_tests.py
+++ 2022/day7_tests.py
@@ -3,7 +3,7 @@ import itertools
 import unittest
 from typing import Iterator
 
-from day7 import parse_lines
+from day7 import parse_lines, part_one, part_two
 
 
 class TestDay7(unittest.TestCase):
@@ -33,9 +33,22 @@ $ ls
 7214296 k"""
 
     def test_part_one(self):
-        parse_lines(self.example1.splitlines())
-        # self.assertEqual(find_signal(self.example1), 5)
-        self.assertTrue(True)
+        # part_one(self.example1.splitlines())
+        # root = parse_lines(self.example1.splitlines())
+
+        # print(root.get_recursive_size())
+        # # self.assertEqual(find_signal(self.example1), 5)
+
+        # big_dirs = [d for d in root.dfs() if d.get_recursive_size() < 100000]
+        # print(sum([d.get_recursive_size() for d in big_dirs]))
+        # for d in root.dfs():
+        # print(f"Traversing directory {d.name}")
+
+        self.assertEqual(part_one(self.example1.splitlines()), 95437)
+
+    def test_part_two(self):
+        self.assertEqual(part_two(self.example1.splitlines()), 24933642)
+        # self.assertTrue(True)
 
 
 if __name__ == "__main__":
