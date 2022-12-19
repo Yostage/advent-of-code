3e49869bc78bd59b5f730bc639d6eed9607e5611
Scott Yost <scott@yostage.com>
2022-12-19 15:52:52 -0800

    make example work in template

--- 2022/day_template_tests.py
+++ 2022/day_template_tests.py
@@ -8,19 +8,18 @@ from day9 import parse_lines, part_one, part_two
 
 class TestDay9(unittest.TestCase):
 
-    example = """
-    """
+    example = """"""
 
     def test_parse_lines(self):
         parse_lines(self.example.splitlines())
 
     def test_part_one(self):
         result = part_one(self.example.splitlines())
-        # self.assertEqual(result, 21)
+        # self.assertEqual(result, xx)
 
     def test_part_two(self):
         result = part_two(self.example.splitlines())
-        # self.assertEqual(result, 21)
+        # self.assertEqual(result, xx)
 
 
 if __name__ == "__main__":
