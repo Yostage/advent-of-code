90b981dda2db840a9261f8c6eb66deb948be7834
Scott Yost <scott@yostage.com>
2022-12-25 14:45:37 -0800

    refactor out CallCounter

--- 2022/day16.py
+++ 2022/day16.py
@@ -9,40 +9,7 @@ from typing import Any, Dict, Iterable, List, Set, Tuple, TypeVar
 
 import more_itertools
 
-
-class CallCounter(object):
-    "Decorator that keeps track of the number of times a function is called."
-
-    __instances = {}  # type: ignore
-
-    def __init__(self, f):
-        self.__f = f
-        self.__numcalls = 0
-        CallCounter.__instances[f] = self
-
-    def __call__(self, *args, **kwargs):
-        self.__numcalls += 1
-        return self.__f(*args, **kwargs)
-
-    # @staticmethod
-    def count_calls(self):
-        "Return the number of times the function f was called."
-        return CallCounter.__instances[self.__f].__numcalls
-
-    @staticmethod
-    def clear():
-        for f in CallCounter.__instances:
-            CallCounter.__instances[f].__numcalls = 0
-
-    @staticmethod
-    def counts():
-        "Return a dict of {function: # of calls} for all registered functions."
-        return dict(
-            [
-                (f.__name__, CallCounter.__instances[f].__numcalls)
-                for f in CallCounter.__instances
-            ]
-        )
+from CallCounter import CallCounter
 
 
 @dataclass
@@ -99,6 +66,7 @@ def print_graph(v: ValveDict) -> None:
 
 
 bfs_cache: Dict[Tuple[str, str], int] = {}
+
 
 @CallCounter
 def bfs_shortest_distance(valves: ValveDict, start_at: str, dest: str) -> int:
