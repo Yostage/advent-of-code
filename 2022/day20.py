5ab7f4b5cecf84cdf6890cce79f7e9d6cb1663d9
Scott Yost <scott@yostage.com>
2022-12-29 21:09:08 -0800

    20.1

--- 2022/day20.py
+++ 2022/day20.py
@@ -2,16 +2,106 @@ import functools
 import re
 from dataclasses import dataclass, field
 from functools import cache
+from operator import attrgetter
 from typing import Any, Dict, List, TypeVar
 
+from day13 import sign
 
-def parse_lines(lines: List[str]) -> Any:
-    return None
+
+@dataclass
+class MixableNumber:
+    val: int
+    pos: int
+
+
+def parse_lines(lines: List[str]) -> List[MixableNumber]:
+    return [MixableNumber(int(line), idx) for idx, line in enumerate(lines)]
+
+
+def mix(mx: MixableNumber, numbers: List[MixableNumber]) -> None:
+    ring = len(numbers)
+    old_loc = mx.pos
+    # ring - 1 because the being last and being first are the same. there's
+    # one fewer slot than you think there are
+    new_loc = (mx.val + mx.pos) % (ring - 1)
+
+    if old_loc == new_loc:
+        return
+
+    # print(f"{mx.val} moves from {old_loc} to {new_loc}")
+    mx.pos = new_loc
+    for num in numbers:
+        # we already processed this
+        if num is mx:
+            continue
+
+        # we always need to visit new, because somebody is still there.
+        # we don't need to visit old because we evacuated it
+
+        # we moved backwards, everybody shift up
+        if new_loc < old_loc:
+            if new_loc <= num.pos and num.pos < old_loc:
+                num.pos += 1
+        # everybody shift back
+        if old_loc < new_loc:
+            if old_loc < num.pos and num.pos <= new_loc:
+                num.pos -= 1
+
+
+def val_at_pos(pos: int, numbers: List[MixableNumber]) -> int:
+    ring = len(numbers)
+    pos = pos % ring
+    for mx in numbers:
+        if mx.pos == pos:
+            return mx.val
+    assert False
+
+
+def verify_integrity(numbers: List[MixableNumber]) -> None:
+    positions: Dict[int, MixableNumber] = dict()
+    for mx in numbers:
+        if mx.pos in positions:
+            raise AssertionError(f"{mx} and {positions[mx.pos]} collision")
+        positions[mx.pos] = mx
+
+    ring = len(numbers)
+    assert len(positions) == ring
+    assert min(positions.keys()) == 0
+    assert max(positions.keys()) == ring - 1
+
+
+def find_val(val: int, numbers: List[MixableNumber]) -> int:
+    for mx in numbers:
+        if mx.val == val:
+            return mx.pos
+    assert False
+
+
+def printable_vals(numbers: List[MixableNumber]) -> str:
+    s = sorted(numbers, key=attrgetter("pos"))
+    return ", ".join(str(mx.val) for mx in s)
 
 
 def part_one(lines) -> int:
-    parse_lines(lines)
-    return 0
+
+    # mix every number
+    # 1000th, 2000th, and 3000th numbers after the value 0
+    nums = parse_lines(lines)
+
+    # print(printable_vals(nums))
+    # print("Mixing")
+    for num in nums:
+        mix(num, nums)
+        verify_integrity(nums)
+        # print(printable_vals(nums))
+
+    zero_pos = find_val(0, nums)
+    print(f"The zero was at pos {zero_pos}")
+    for offset in [1000, 2000, 3000]:
+        print(
+            f"Value at {offset+zero_pos}({(offset+zero_pos)% len(nums)}) is {val_at_pos(offset+zero_pos,nums)}"
+        )
+    return sum((val_at_pos(offset + zero_pos, nums) for offset in [1000, 2000, 3000]))
 
 
 def part_two(lines) -> int:
@@ -23,9 +113,8 @@ def main() -> None:
     with open("day20_input.txt", "r") as file:
         lines = file.read().splitlines()
         print(part_one(lines))
-        print(part_two(lines))
+        # print(part_two(lines))
 
 
 if __name__ == "__main__":
     main()
-
--- 2022/day20_tests.py
+++ 2022/day20_tests.py
@@ -3,7 +3,15 @@ import itertools
 import unittest
 from typing import Iterator
 
-from day20 import parse_lines, part_one, part_two
+from day20 import (
+    MixableNumber,
+    mix,
+    parse_lines,
+    part_one,
+    part_two,
+    printable_vals,
+    verify_integrity,
+)
 
 
 class TestDay20(unittest.TestCase):
@@ -17,11 +25,69 @@ class TestDay20(unittest.TestCase):
 4"""
 
     def test_parse_lines(self):
-        parse_lines(self.example.splitlines())
+        nums = parse_lines(self.example.splitlines())
+        self.assertEqual(len(nums), 7)
+
+    def test_equality(self):
+        one = MixableNumber(0, 0)
+        two = MixableNumber(0, 2)
+        one_p = MixableNumber(0, 0)
+        self.assertNotEqual(one, two)
+        self.assertTrue(one is not one_p)
+        self.assertTrue(one is one)
+
+    def test_mix(self):
+        nums = parse_lines(["1", "2", "3"])
+        self.assertEqual(printable_vals(nums), "1, 2, 3")
+        mix(nums[0], nums)
+        verify_integrity(nums)
+        self.assertEqual(printable_vals(nums), "2, 1, 3")
+
+        nums = parse_lines(["3", "2", "1"])
+        self.assertEqual(printable_vals(nums), "3, 2, 1")
+        mix(nums[-1], nums)
+        verify_integrity(nums)
+        self.assertEqual(printable_vals(nums), "3, 1, 2")
+
+        nums = parse_lines(["-1", "2", "3"])
+        self.assertEqual(printable_vals(nums), "-1, 2, 3")
+        mix(nums[0], nums)
+        verify_integrity(nums)
+        self.assertEqual(printable_vals(nums), "2, -1, 3")
+
+        nums = parse_lines(["1", "1"])
+        self.assertEqual(printable_vals(nums), "1, 1")
+        mix(nums[0], nums)
+        verify_integrity(nums)
+        self.assertEqual(printable_vals(nums), "1, 1")
+
+        nums = parse_lines(["1", "2", "3"])
+        mix(nums[0], nums)
+        verify_integrity(nums)
+        self.assertEqual(printable_vals(nums), "2, 1, 3")
+
+        nums = parse_lines(["3", "2", "3"])
+        mix(nums[0], nums)
+        verify_integrity(nums)
+        self.assertEqual(printable_vals(nums), "2, 3, 3")
+
+        nums = parse_lines(["5", "2", "3"])
+        mix(nums[0], nums)
+        verify_integrity(nums)
+        self.assertEqual(printable_vals(nums), "2, 5, 3")
+
+    def test_full_mix(self):
+        nums = parse_lines(self.example.splitlines())
+        for num in nums:
+            mix(num, nums)
+
+        self.assertEqual(printable_vals(nums), "-2, 1, 2, -3, 4, 0, 3")
+        # we don't match their start point but we do match their ordering
+        # self.assertEqual(printable_vals(nums), "1, 2, -3, 4, 0, 3, -2")
 
     def test_part_one(self):
         result = part_one(self.example.splitlines())
-        # self.assertEqual(result, xx)
+        self.assertEqual(result, 3)
 
     def test_part_two(self):
         result = part_two(self.example.splitlines())
