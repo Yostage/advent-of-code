c9b6b171691be3767b39ea2722d923a1ace56c16
Scott Yost <scott@yostage.com>
2023-01-03 10:37:27 -0800

    25.1

--- 2022/day25.py
+++ 2022/day25.py
@@ -4,28 +4,72 @@ from dataclasses import dataclass, field
 from functools import cache
 from typing import Any, Dict, List, TypeVar
 
-
-def parse_lines(lines: List[str]) -> Any:
-    return None
+SnafuEncoded = str
 
 
-def part_one(lines) -> int:
-    parse_lines(lines)
-    return 0
+int_to_snafu_encodings = {
+    2: "2",
+    1: "1",
+    0: "0",
+    -1: "-",
+    -2: "=",
+}
+
+snafu_to_int_encodings = {v: k for k, v in int_to_snafu_encodings.items()}
 
 
-def part_two(lines) -> int:
-    parse_lines(lines)
-    return 0
+def int_to_snafu(input: int) -> SnafuEncoded:
+    if input == 0:
+        return "0"
+    base = 5
+    encodings = {
+        2: "2",
+        1: "1",
+        0: "0",
+        -1: "-",
+        -2: "=",
+    }
+    enc = [0, 1, 2, -2, -1]
+    total = input
+    place = 0
+    output = []
+    while total != 0 and place < 10000:
+        this_digit = (total // (base**place)) % (base)
+        # but, surprise, we can't actually write the digit we wanted
+        output.append(encodings[enc[this_digit]])
+        # so handle that we wrote the digit we had instead and keep going
+        total -= enc[this_digit] * (base**place)
+        place += 1
+
+    return "".join(reversed(output))
+
+
+def snafu_to_int(input: str) -> int:
+    base = 5
+    return sum(
+        snafu_to_int_encodings[digit] * (base**idx)
+        for idx, digit in enumerate(reversed(list(input)))
+    )
+
+
+def parse_lines(lines: List[str]) -> List[str]:
+    return lines
+
+
+def part_one(lines) -> str:
+    snafus = parse_lines(lines)
+    total = sum(snafu_to_int(s) for s in snafus)
+    return int_to_snafu(total)
+    # for x in range(20):
+    #     print(f"{x} => {int_to_snafu(x)} => {snafu_to_int(int_to_snafu(x))}")
+    # return 0
 
 
 def main() -> None:
     with open("day25_input.txt", "r") as file:
         lines = file.read().splitlines()
         print(part_one(lines))
-        print(part_two(lines))
 
 
 if __name__ == "__main__":
     main()
-
--- 2022/day25_tests.py
+++ 2022/day25_tests.py
@@ -3,7 +3,7 @@ import itertools
 import unittest
 from typing import Iterator
 
-from day25 import parse_lines, part_one, part_two
+from day25 import int_to_snafu, parse_lines, part_one, snafu_to_int
 
 
 class TestDay25(unittest.TestCase):
@@ -25,13 +25,27 @@ class TestDay25(unittest.TestCase):
     def test_parse_lines(self):
         parse_lines(self.example.splitlines())
 
+    examples = [
+        (1, "1"),
+        (2, "2"),
+        (5, "10"),
+        (10, "20"),
+        (2022, "1=11-2"),
+        (12345, "1-0---0"),
+        (314159265, "1121-1110-1=0"),
+    ]
+
+    def test_int_to_snafu(self):
+        for (integer, snafu) in self.examples:
+            self.assertEqual(snafu, int_to_snafu(integer))
+
+    def test_snafu_to_int(self):
+        for (integer, snafu) in self.examples:
+            self.assertEqual(integer, snafu_to_int(snafu))
+
     def test_part_one(self):
         result = part_one(self.example.splitlines())
-        # self.assertEqual(result, xx)
-
-    def test_part_two(self):
-        result = part_two(self.example.splitlines())
-        # self.assertEqual(result, xx)
+        self.assertEqual(result, "2=-1=0")
 
 
 if __name__ == "__main__":
