f1cd0a52cc75b3570713056f449952d70a7fc41e
Scott Yost <scott@yostage.com>
2023-12-01 21:15:05 -0800

    day one complete

--- 2023/day01.py
+++ 2023/day01.py
@@ -5,18 +5,52 @@ from functools import cache
 from typing import Any, Dict, List, TypeVar
 
 
-def parse_lines(lines: List[str]) -> Any:
-    return None
+def parse_lines(lines: List[str]) -> List[str]:
+    return lines
 
 
 def part_one(lines) -> int:
-    parse_lines(lines)
-    return 0
+    lines = parse_lines(lines)
+    sum = 0
+    for line in lines:
+        first_digit = next(filter(lambda c: c.isdigit(), line))
+        last_digit = next(filter(lambda c: c.isdigit(), reversed(line)))
+        sum += 10 * int(first_digit) + int(last_digit)
+
+    return sum
+
+
+welp = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
 
 
 def part_two(lines) -> int:
-    parse_lines(lines)
-    return 0
+    def downshift(c: str) -> int:
+        if c.isdigit():
+            return int(c)
+        return welp[c]
+
+    lines = parse_lines(lines)
+    sum = 0
+    pattern = r"\d" + "|" + "one|two|three|four|five|six|seven|eight|nine"
+    reversed_pattern = "\d" + "|" + "one|two|three|four|five|six|seven|eight|nine"[::-1]
+    for line in lines:
+        digits = re.findall(pattern, line)
+        sum += 10 * downshift(digits[0])
+        digits = re.findall(reversed_pattern, line[::-1])
+        sum += downshift(digits[0][::-1])
+        # print(f"input: [{line}] -> [{digits[0]}], [{digits[-1]}]")
+
+    return sum
 
 
 def main() -> None:
@@ -28,4 +62,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
--- 2023/day01_tests.py
+++ 2023/day01_tests.py
@@ -7,21 +7,30 @@ from day01 import parse_lines, part_one, part_two
 
 
 class TestDay01(unittest.TestCase):
+    example = """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet"""
 
-    example = """"""
+    example2 = """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen"""
 
     def test_parse_lines(self):
         parse_lines(self.example.splitlines())
 
     def test_part_one(self):
         result = part_one(self.example.splitlines())
-        # self.assertEqual(result, xx)
+        self.assertEqual(result, 142)
 
     def test_part_two(self):
-        result = part_two(self.example.splitlines())
-        # self.assertEqual(result, xx)
+        result = part_two(self.example2.splitlines())
+        self.assertEqual(result, 281)
 
 
 if __name__ == "__main__":
     unittest.main()
-
