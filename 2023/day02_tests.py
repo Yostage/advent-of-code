623cf0fa27fb8486e9a47dc8d80c67affa8a20d3
Scott Yost <scott@yostage.com>
2023-12-03 16:07:39 -0800

    day2.0

---
+++ 2023/day02_tests.py
@@ -0,0 +1,29 @@
+import functools
+import itertools
+import unittest
+from typing import Iterator
+
+from day02 import parse_lines, part_one, part_two
+
+
+class TestDay02(unittest.TestCase):
+    example = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+
+    def test_parse_lines(self):
+        parse_lines(self.example.splitlines())
+
+    def test_part_one(self):
+        result = part_one(self.example.splitlines())
+        # self.assertEqual(result, xx)
+
+    def test_part_two(self):
+        result = part_two(self.example.splitlines())
+        # self.assertEqual(result, xx)
+
+
+if __name__ == "__main__":
+    unittest.main()
