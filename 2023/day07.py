829d906903c8cdbe942bfc2e8d239d1f1e1008a3
Scott Yost <scott@yostage.com>
2023-12-10 18:18:53 -0800

    day07.1

--- 2023/day07.py
+++ 2023/day07.py
@@ -1,17 +1,76 @@
 import functools
 import re
 from dataclasses import dataclass, field
-from functools import cache
-from typing import Any, Dict, List, TypeVar
+from functools import cache, cached_property
+from itertools import groupby
+from operator import methodcaller
+from typing import Any, Dict, List, Tuple, TypeVar
+
+FiveCardHand = Tuple[int, int, int, int, int]
 
 
-def parse_lines(lines: List[str]) -> Any:
-    return None
+@dataclass
+class CamelCard:
+    hand: FiveCardHand
+    bid: int
+
+    @cached_property
+    def handrank(self) -> int:
+        histogram = {k: len(list(g)) for k, g in groupby(sorted(self.hand))}
+        frequencies = sorted(histogram.values())
+        if frequencies == [5]:
+            return 0
+        if frequencies == [1, 4]:
+            return -1
+        if frequencies == [2, 3]:
+            return -2
+        if frequencies == [1, 1, 3]:
+            return -3
+        if frequencies == [1, 2, 2]:
+            return -4
+        if frequencies == [1, 1, 1, 2]:
+            return -5
+        assert frequencies == [1, 1, 1, 1, 1]
+        return -6
+
+    def sortkey(self) -> Tuple[int, int, int, int, int, int]:
+        return (self.handrank, *self.hand)
+
+
+def card_to_rank(c: str):
+    values = {
+        "A": 14,
+        "K": 13,
+        "Q": 12,
+        "J": 11,
+        "T": 10,
+    }
+    if c.isdigit():
+        return int(c)
+    return values[c]
+
+
+def parse_lines(lines: List[str]) -> List[CamelCard]:
+    cards: List[CamelCard] = []
+    for line in lines:
+        (hand, bid) = line.split(" ")
+        cards.append(CamelCard(hand=list([card_to_rank(c) for c in hand]), bid=int(bid)))  # type: ignore
+    return cards
 
 
 def part_one(lines) -> int:
-    parse_lines(lines)
-    return 0
+    cards = parse_lines(lines)
+    return sum(
+        [
+            card.bid * (idx + 1)
+            for idx, card in enumerate(sorted(cards, key=methodcaller("sortkey")))
+        ]
+    )
+    # sum = 0
+    # for idx, card in enumerate(sorted(cards, key=methodcaller("sortkey"))):
+    #     sum += card.bid * (idx + 1)
+    #     print(f"{card}: handrank {card.handrank}: rank {idx+1}")
+    # return sum
 
 
 def part_two(lines) -> int:
@@ -28,4 +87,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
