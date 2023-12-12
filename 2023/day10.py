b6ea08bcbaad64f5282dcbf855c594c704d05f41
Scott Yost <scott@yostage.com>
2023-12-11 21:14:16 -0800

    day10.1

--- 2023/day10.py
+++ 2023/day10.py
@@ -1,17 +1,87 @@
 import functools
 import re
+from collections import defaultdict, deque
 from dataclasses import dataclass, field
 from functools import cache
-from typing import Any, Dict, List, TypeVar
+from typing import Any, Deque, Dict, List, Mapping, Sequence, Set, TypeVar
+
+from util import Point2D, tuple2_add
+
+adjacencies = {
+    "|": ((0, 1), (0, -1)),
+    "-": ((1, 0), (-1, 0)),
+    "L": ((0, -1), (1, 0)),
+    "J": ((0, -1), (-1, 0)),
+    "7": ((0, 1), (-1, 0)),
+    "F": ((0, 1), (1, 0)),
+    ".": tuple(),
+}
+
+
+@dataclass
+class PipeMaze:
+    start: Point2D
+    map: Mapping[Point2D, Sequence[Point2D]]
 
 
 def parse_lines(lines: List[str]) -> Any:
-    return None
+    map: Dict[Point2D, Sequence[Point2D]] = {}
+    start = None
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if c == "S":
+                start = (x, y)
+                map[(x, y)] = []
+            else:
+                # convert the adjacencies to actual edges
+                map[(x, y)] = [tuple2_add((x, y), adj) for adj in adjacencies[c]]
+    assert start is not None
+    return PipeMaze(start=start, map=map)
 
 
 def part_one(lines) -> int:
-    parse_lines(lines)
-    return 0
+    maze = parse_lines(lines)
+    # first calculate the shape of our starting place
+    for orthogonal_dir in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+        # backlink to anybody who links to us
+        adjacent_space = tuple2_add(maze.start, orthogonal_dir)
+        if adjacent_space in maze.map and maze.start in maze.map[adjacent_space]:
+            maze.map[maze.start].append(adjacent_space)
+
+    # better be only two links out
+    assert len(maze.map[maze.start]) == 2
+
+    # now visit all the spaces until we're done, and fetch the highest space
+    max_int = 99999999999
+    distances: Dict[Point2D, int] = defaultdict(lambda: max_int)
+    unvisited: Deque = deque()
+    unvisited.append(maze.start)
+    distances[maze.start] = 0
+    visited: Set[Point2D] = set()
+
+    def pretty_cell(x, y, visiting):
+        if distances[(x, y)] == max_int:
+            return "."
+        if (x, y) == visiting:
+            return "*"
+        return str(distances[(x, y)])
+
+    while len(unvisited) > 0:
+        visiting = unvisited.pop()
+        visited.add(visiting)
+        for adjacent in maze.map[visiting]:
+            distances[adjacent] = min(distances[adjacent], distances[visiting] + 1)
+            if adjacent not in visited and adjacent not in unvisited:
+                unvisited.appendleft(adjacent)
+
+    #     for y in range(len(lines)):
+    #         print("".join([pretty_cell(x, y, visiting) for x in range(len(lines[0]))]))
+    #     print()
+
+    # for y in range(len(lines)):
+    #     print("".join([pretty_cell(x, y, (-1, -1)) for x in range(len(lines[0]))]))
+
+    return max([x for x in distances.values() if x != max_int])
 
 
 def part_two(lines) -> int:
@@ -28,4 +98,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
--- 2023/day10_tests.py
+++ 2023/day10_tests.py
@@ -1,6 +1,7 @@
 import functools
 import itertools
 import unittest
+from textwrap import dedent
 from typing import Iterator
 
 from day10 import parse_lines, part_one, part_two
@@ -19,6 +20,60 @@ SJ.L7
 |F--J
 LJ..."""
 
+    example3 = dedent(
+        """\
+...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+..........."""
+    )
+
+    example4 = dedent(
+        """\
+..........
+.S------7.
+.|F----7|.
+.||....||.
+.||....||.
+.|L-7F-J|.
+.|..||..|.
+.L--JL--J.
+.........."""
+    )
+
+    example5 = dedent(
+        """\
+.F----7F7F7F7F-7....
+.|F--7||||||||FJ....
+.||.FJ||||||||L7....
+FJL7L7LJLJ||LJ.L-7..
+L--J.L7...LJS7F-7L7.
+....F-J..F7FJ|L7L7L7
+....L7.F7||L7|.L7L7|
+.....|FJLJ|FJ|F7|.LJ
+....FJL-7.||.||||...
+....L---J.LJ.LJLJ..."""
+    )
+
+    example6 = dedent(
+        """\
+FF7FSF7F7F7F7F7F---7
+L|LJ||||||||||||F--J
+FL-7LJLJ||||||LJL-77
+F--JF--7||LJLJ7F7FJ-
+L---JF-JLJ.||-FJLJJ7
+|F|F-JF---7F7-L7L|7|
+|FFJF7L7F-JF7|JL---7
+7-L-JL7||F7|L7F-7F7|
+L.L7LFJ|||||FJL7||LJ
+L7JLJL-JLJLJL--JLJ.L"""
+    )
+
     def test_parse_lines(self):
         parse_lines(self.example.splitlines())
 
@@ -28,8 +83,10 @@ LJ..."""
         self.assertEqual(part_one(self.example2.splitlines()), 8)
 
     def test_part_two(self):
-        result = part_two(self.example.splitlines())
-        # self.assertEqual(result, xx)
+        self.assertEqual(part_two(self.example3.splitlines()), 4)
+        self.assertEqual(part_two(self.example4.splitlines()), 4)
+        self.assertEqual(part_two(self.example5.splitlines()), 8)
+        self.assertEqual(part_two(self.example6.splitlines()), 10)
 
 
 if __name__ == "__main__":
