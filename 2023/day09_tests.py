c5310bb3431b64ca9fba64e4b4009894bf9bc5df
Scott Yost <scott@yostage.com>
2023-12-11 17:45:11 -0800

    day09.0

---
+++ 2023/day09_tests.py
@@ -0,0 +1,27 @@
+import functools
+import itertools
+import unittest
+from typing import Iterator
+
+from day09 import parse_lines, part_one, part_two
+
+
+class TestDay09(unittest.TestCase):
+    example = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+
+    def test_parse_lines(self):
+        parse_lines(self.example.splitlines())
+
+    def test_part_one(self):
+        result = part_one(self.example.splitlines())
+        self.assertEqual(result, 114)
+
+    def test_part_two(self):
+        result = part_two(self.example.splitlines())
+        # self.assertEqual(result, xx)
+
+
+if __name__ == "__main__":
+    unittest.main()
