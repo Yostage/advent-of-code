b4b0ab1e5dfcbc49474a55e2e6032acf4e0dc73e
Scott Yost <scott@yostage.com>
2023-12-29 23:21:23 -0800

    14.0

---
+++ 2023/day14_tests.py
@@ -0,0 +1,34 @@
+import functools
+import itertools
+import unittest
+from typing import Iterator
+
+from day14 import parse_lines, part_one, part_two
+
+
+class TestDay14(unittest.TestCase):
+    example = """O....#....
+O.OO#....#
+.....##...
+OO.#O....O
+.O.....O#.
+O.#..O.#.#
+..O..#O..O
+.......O..
+#....###..
+#OO..#...."""
+
+    def test_parse_lines(self):
+        parse_lines(self.example.splitlines())
+
+    def test_part_one(self):
+        result = part_one(self.example.splitlines())
+        self.assertEqual(result, 136)
+
+    def test_part_two(self):
+        result = part_two(self.example.splitlines())
+        # self.assertEqual(result, xx)
+
+
+if __name__ == "__main__":
+    unittest.main()
