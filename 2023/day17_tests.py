7a3a2c1a9e78fe4d748ad41860996b0461db5962
Scott Yost <scott@yostage.com>
2024-01-01 21:24:14 -0800

    17.0

---
+++ 2023/day17_tests.py
@@ -0,0 +1,37 @@
+import functools
+import itertools
+import unittest
+from typing import Iterator
+
+from day17 import parse_lines, part_one, part_two
+
+
+class TestDay17(unittest.TestCase):
+    example = """2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533"""
+
+    def test_parse_lines(self):
+        parse_lines(self.example.splitlines())
+
+    def test_part_one(self):
+        result = part_one(self.example.splitlines())
+        self.assertEqual(result, 102)
+
+    def test_part_two(self):
+        result = part_two(self.example.splitlines())
+        # self.assertEqual(result, xx)
+
+
+if __name__ == "__main__":
+    unittest.main()
