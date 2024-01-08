1d969454ee7a91cebcbff836b759d0f6bba1aa80
Scott Yost <scott@yostage.com>
2024-01-07 23:12:45 -0800

    18.2 DO_NOT_USE_OR_YOU_WILL_BE_FIRED

--- 2023/day18.py
+++ 2023/day18.py
@@ -3,16 +3,21 @@ import re
 from collections import defaultdict, deque
 from dataclasses import dataclass, field
 from functools import cache
-from typing import Any, Deque, Dict, List, Set, TypeVar
+from itertools import groupby
+from typing import Any, Deque, Dict, List, Set, Tuple, TypeVar
 
 from CharacterGrid import CharacterGrid, Directions, orthogonal_adjacencies
-from util import Point2D, tuple2_add
+from util import Point2D, tuple2_add, tuple2_scalar_mul
 
 dmap = {
     "U": Directions.UP,
     "D": Directions.DOWN,
     "L": Directions.LEFT,
     "R": Directions.RIGHT,
+    "3": Directions.UP,
+    "1": Directions.DOWN,
+    "2": Directions.LEFT,
+    "0": Directions.RIGHT,
 }
 
 
@@ -104,9 +109,180 @@ def part_one(lines) -> int:
     return sum(1 for x in grid.map.values() if x not in ("o", "."))
 
 
-def part_two(lines) -> int:
-    parse_lines(lines)
-    return 0
+def union_interval_length(intervals: List[Point2D]) -> int:
+    if not intervals:
+        return 0
+
+    sorted_intervals = list(sorted(intervals))
+
+    result = [sorted_intervals[0]]
+
+    for interval in sorted_intervals[1:]:
+        tail = result[-1]
+        # we're extending this interval
+        if tail[1] == interval[0]:
+            result[-1] = (tail[0], interval[1])
+        # the interval intersects
+        elif tail[0] < interval[0] and tail[1] > interval[1]:
+            continue
+        # we're truncating the beginning
+        elif tail[0] == interval[0] and interval[1] != tail[1]:
+            result[-1] = (tail[0], interval[1])
+        # we're truncating the end
+        elif tail[0] < interval[0] and interval[1] == tail[1]:
+            continue
+        else:
+            result.append(interval)
+
+    totes = sum(x[1] - x[0] + 1 for x in result)
+    print(f"\tUnioned intervals: {result}, we have {totes}")
+
+    return totes
+
+
+def coalesce_intervals(intervals: List[Point2D]) -> Tuple[List[Point2D], int]:
+    if not intervals:
+        return (list(), 0)
+
+    # Sort intervals based on start values
+    sorted_intervals = list(sorted(intervals))
+    print(f"\tCoalescing {sorted_intervals}")
+
+    result = [sorted_intervals[0]]
+    extra_width = 0
+
+    for interval in sorted_intervals[1:]:
+        tail = result[-1]
+        # we're extending this interval
+        if tail[1] == interval[0]:
+            result[-1] = (tail[0], interval[1])
+        # the interval intersects
+        elif tail[0] < interval[0] and tail[1] > interval[1]:
+            result[-1] = (tail[0], interval[0])
+            result.append((interval[1], tail[1]))
+            extra_width += interval[1] - interval[0]
+        # we're truncating the beginning
+        elif tail[0] == interval[0] and interval[1] != tail[1]:
+            result[-1] = (tail[1], interval[1])
+            extra_width += tail[1] - tail[0]
+        # we're truncating the end
+        elif tail[0] < interval[0] and interval[1] == tail[1]:
+            result[-1] = (tail[0], interval[0])
+        elif tail == interval:
+            # count the entire interval, because this is the only time we see it
+            # inclusive
+            extra_width += (interval[1] - interval[0]) + 1
+            del result[-1]
+
+        else:
+            # No overlap, add the interval to the result
+            result.append(interval)
+        print(f"\tAfter {interval}, we have {result}")
+
+    return (result, extra_width)
+
+
+def parse_colored_lines(lines: List[str]) -> List[Tuple[Point2D, Point2D]]:
+    # parse the input into strokes
+    strokes = []
+    for line in parse_lines(lines):
+        (_, _, color) = line
+        dir = dmap[color[-1]]
+        length = int(color[:-1], 16)
+        strokes.append((dir, length))
+        # print(f"dir: {dir} length: {length}")
+    start = (0, 0)
+    # now turn the strokes into segments
+    segments = []
+    for stroke in strokes:
+        end = tuple2_add(start, tuple2_scalar_mul(stroke[0], stroke[1]))
+        segments.append((start, end))
+        start = end
+
+    # we better get back to start
+    assert start == (0, 0)
+    return segments
+
+
+def parse_regular_lines(lines: List[str]) -> List[Tuple[Point2D, Point2D]]:
+    # lines = parse_lines(lines)
+    # parse the input into strokes
+
+    strokes = []
+    for line in parse_lines(lines):
+        (dir, length, _) = line
+        # dir = dmap[color[-1]]
+        # length = int(color[:-1], 16)
+        strokes.append((dmap[dir], int(length)))
+        # print(f"dir: {dir} length: {length}")
+    start = (0, 0)
+    # now turn the strokes into segments
+    segments = []
+    for stroke in strokes:
+        end = tuple2_add(start, tuple2_scalar_mul(stroke[0], stroke[1]))
+        segments.append((start, end))
+        start = end
+
+    # we better get back to start
+    assert start == (0, 0)
+    return segments
+
+
+def part_two(lines, legacy: bool = False) -> int:
+    if legacy:
+        segments = parse_regular_lines(lines)
+    else:
+        segments = parse_colored_lines(lines)
+
+    for segment in segments:
+        print(f"{segment[0]} -> {segment[1]}")
+
+    # alright time to start counting
+    horizontal_slices = filter(lambda seg: seg[0][1] == seg[1][1], segments)
+    by_y = groupby(
+        sorted(horizontal_slices, key=lambda s: s[0][1]), lambda seg: seg[0][1]
+    )
+
+    total_dug = 0
+
+    open_intervals: List[Point2D] = []
+    prev_y = None
+    for y, segs in by_y:
+        # accumulate width * dy
+        if prev_y is not None:
+            width = sum(1 + x[1] - x[0] for x in open_intervals)
+            print(
+                f"{total_dug} + {width} * ({y}-{prev_y}-1) = {total_dug + width * (y - prev_y -1)}"
+            )
+            total_dug += width * (y - prev_y - 1)
+
+        # we don't need the y anymore
+        x_intervals: List[Point2D] = [
+            tuple(sorted((seg[0][0], seg[1][0]))) for seg in segs  # type:ignore
+        ]
+        print(f"{y}: {x_intervals}")
+        this_row_width = 0
+        for i in x_intervals:
+            if i in open_intervals:
+                open_intervals.remove(i)
+                # still counts
+                this_row_width += (i[1] - i[0]) + 1
+            else:
+                open_intervals.append(i)
+
+        extra_width = union_interval_length(open_intervals)
+        (open_intervals, _) = coalesce_intervals(open_intervals)  # type: ignore
+        this_row_width += extra_width
+        print(f"Open status: {open_intervals}")
+
+        print(
+            f"Adding this row's width. {this_row_width}. Total now {total_dug +  this_row_width}"
+        )
+        total_dug += this_row_width
+
+        prev_y = y
+
+    return total_dug
 
 
 def main() -> None:
--- 2023/day18_tests.py
+++ 2023/day18_tests.py
@@ -23,16 +23,46 @@ L 2 (#015232)
 U 2 (#7a21e3)
 """
 
+    example2 = """R 6 (#70c710)
+D 4 (#0dc571)
+L 2 (#5713f0)
+U 2 (#d2c081)
+L 2 (#59c680)
+D 4 (#411b91)
+L 2 (#8ceee2)
+U 6 (#caa173)
+"""
+
+    example3 = """D 4 (#70c710)
+R 6 (#0dc571)
+U 6 (#5713f0)
+L 2 (#59c680)
+D 4 (#411b91)
+L 2 (#8ceee2)
+U 2 (#8ceee2)
+L 2 (#8ceee2)
+"""
+
     def test_parse_lines(self):
         parse_lines(self.example.splitlines())
 
     def test_part_one(self):
         result = part_one(self.example.splitlines())
         self.assertEqual(result, 62)
+        result = part_one(self.example2.splitlines())
+        self.assertEqual(result, 39)
+        result = part_one(self.example3.splitlines())
+        self.assertEqual(result, 39)
 
     def test_part_two(self):
         result = part_two(self.example.splitlines())
-        # self.assertEqual(result, xx)
+        self.assertEqual(result, 952408144115)
+        result = part_two(self.example.splitlines(), legacy=True)
+        self.assertEqual(result, 62)
+        result = part_two(self.example2.splitlines(), legacy=True)
+        self.assertEqual(result, 39)
+        result = part_two(self.example3.splitlines(), legacy=True)
+        self.assertEqual(result, 39)
 
 
 if __name__ == "__main__":
