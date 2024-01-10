2e053c6c806afb6d4ce8aa7a4c2e50d48a0bf9ee
Scott Yost <scott@yostage.com>
2024-01-09 18:03:37 -0800

    19.1

--- 2023/day19.py
+++ 2023/day19.py
@@ -4,14 +4,65 @@ from dataclasses import dataclass, field
 from functools import cache
 from typing import Any, Deque, Dict, List, Set, Tuple, TypeVar
 
+part_spec = List[str]
+workflow = List[str]
+# workflow_dict = Dict[str, workflow]
 
-def parse_lines(lines: List[str]) -> Any:
-    return None
+
+def parse_lines(lines: List[str]) -> Tuple[Dict[str, workflow], List[part_spec]]:
+    workflows = {}
+    line_iter = iter(lines)
+    for line in line_iter:
+        if line == "":
+            break
+        (prefix, rest) = line.split("{")
+        workflows[prefix] = rest[:-1].split(",")
+
+    parts = [line[1:-1].split(",") for line in line_iter]
+
+    # qqz{s>2770:qs,m<1801:hdj,R}
+    return (workflows, parts)
 
 
-def part_one(lines) -> int:
-    parse_lines(lines)
-    return 0
+# @cache
+def workflow_to_evaluator(w: workflow) -> str:
+    # s>2770:qs,m<1801:hdj,R
+    evaluators: List[str] = []
+    for step in w:
+        if ":" in step:
+            (predicate, result) = step.split(":")
+            evaluators.append(f"'{result}' if {predicate} else")
+        else:
+            evaluators.append(f"'{step}'")
+    return " ".join(evaluators)
+
+
+def score_part(part: part_spec, workflow_dict: Dict[str, workflow]) -> int:
+    # x = m = a = s = 0
+    # load up all the variables
+    for stat in part:
+        # print(stat)
+        exec(stat)
+
+    next_state = "in"
+    while next_state not in ["A", "R"]:
+        workflow = workflow_dict[next_state]
+        next_state = eval(workflow_to_evaluator(workflow))
+
+    if next_state == "A":
+        return eval("x + m + a + s")
+    else:
+        assert next_state == "R"
+        return 0
+
+
+def part_one(lines: List[str]) -> int:
+    (workflows, parts) = parse_lines(lines)
+    print(workflows)
+    # for w in workflows.values():
+    #     print(workflow_to_evaluator(tuple(w)))
+    # print(parts)
+    return sum(score_part(part, workflows) for part in parts)
 
 
 def part_two(lines) -> int:
@@ -28,4 +79,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
--- 2023/day19_tests.py
+++ 2023/day19_tests.py
@@ -27,7 +27,7 @@ hdj{m>838:A,pv}
 
     def test_part_one(self):
         result = part_one(self.example.splitlines())
-        # self.assertEqual(result, 19114)
+        self.assertEqual(result, 19114)
 
     def test_part_two(self):
         result = part_two(self.example.splitlines())
