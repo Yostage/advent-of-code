93544134d3f2b6b0ba01eee1f3802f1f4645ca86
Scott Yost <scott@yostage.com>
2024-01-15 16:08:28 -0800

    21.1

--- 2023/day21.py
+++ 2023/day21.py
@@ -2,16 +2,42 @@ import functools
 import re
 from dataclasses import dataclass, field
 from functools import cache
+from multiprocessing import Value
 from typing import Any, Deque, Dict, List, Set, Tuple, TypeVar
+
+from CharacterGrid import CharacterGrid, orthogonal_adjacencies
+from util import Point2D, tuple2_add
 
 
 def parse_lines(lines: List[str]) -> Any:
-    return None
+    return CharacterGrid.from_lines(lines)
 
 
 def part_one(lines) -> int:
-    parse_lines(lines)
-    return 0
+    grid = parse_lines(lines)
+
+    if grid.max_x() == 10:
+        steps = 6
+    elif grid.max_x() == 130:
+        steps = 64
+    else:
+        raise ValueError(grid.max_x())
+
+    next_gen: Set[Point2D] = set(k for k, v in grid.map.items() if v == "S")
+    assert len(next_gen) == 1
+    for generation in range(steps):
+        last_gen = next_gen
+        next_gen = set()
+        for space in last_gen:
+            for adj in orthogonal_adjacencies:
+                candidate = tuple2_add(space, adj)
+                if grid.map.get(candidate) in ["S", "."]:
+                    next_gen.add(candidate)
+        # print(
+        #     f"generation:{generation}: from {len(last_gen)} plots to {len(next_gen)} plots"
+        # )
+
+    return len(next_gen)
 
 
 def part_two(lines) -> int:
@@ -28,4 +54,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
--- 2023/day21_tests.py
+++ 2023/day21_tests.py
@@ -2,6 +2,21 @@ import unittest
 
 from day21 import parse_lines, part_one, part_two
 
+# January 7 11:18 PM Drilian: 21 probably worst of the remainders
+# January 7 11:14 PM Drilian: For 21 I will say
+# January 7 11:14 PM Drilian: well, for part 2
+# January 7 11:15 PM Drilian: you're gonna get the usual big fucking number of things to do
+# January 7 11:15 PM Drilian: look at how it is modulo the size of the grid
+# January 7 11:15 PM Drilian: and maybe graph what it looks like when you go THAT far
+# January 7 11:15 PM Drilian: and notice what is going on
+# January 7 11:15 PM Drilian: and it will save you a ton of time
+
+# January 7 11:16 PM Drilian: 22 was fun
+# January 7 11:16 PM Drilian: 23 was tricky but manageably tricky
+# January 7 11:17 PM Drilian: 24 is the one where I was bitter that I did just an absolute shitload of math and a bunch of people just threw it into a python solver library
+# January 7 11:17 PM Drilian: and 25 took me a while to figure out what the fuck even to do but it only has a part 1 because it's 25
+# January 7 11:18 PM Drilian: and I will happily give you time-saving hints if you want them
+
 
 class TestDay21(unittest.TestCase):
     example = """...........
@@ -21,7 +36,7 @@ class TestDay21(unittest.TestCase):
 
     def test_part_one(self):
         result = part_one(self.example.splitlines())
-        # self.assertEqual(result, 16)
+        self.assertEqual(result, 16)
 
     def test_part_two(self):
         result = part_two(self.example.splitlines())
