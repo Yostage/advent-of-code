4bfecea7028fd525b2096b4411aa7e174f3acb32
Scott Yost <scott@yostage.com>
2024-12-08 19:09:08 -0800

    day02.00

---
+++ 2024/day02_tests.py
@@ -0,0 +1,28 @@
+import unittest
+
+from day02 import parse_lines, part_one, part_two
+
+
+class TestDay02(unittest.TestCase):
+    example = """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+"""
+
+    def test_parse_lines(self):
+        parse_lines(self.example.splitlines())
+
+    def test_part_one(self):
+        result = part_one(self.example.splitlines())
+        # self.assertEqual(result, 2)
+
+    def test_part_two(self):
+        result = part_two(self.example.splitlines())
+        # self.assertEqual(result, xx)
+
+
+if __name__ == "__main__":
+    unittest.main()
