7f6a43e573f49f5fbee5571f997889f39c7833e8
Scott Yost <scott@yostage.com>
2024-12-21 11:44:40 -0800

    day04

---
+++ 2024/day04_tests.py
@@ -0,0 +1,31 @@
+import unittest
+
+from day04 import parse_lines, part_one, part_two
+
+
+class TestDay04(unittest.TestCase):
+    example = """MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"""
+
+    def test_parse_lines(self):
+        parse_lines(self.example.splitlines())
+
+    def test_part_one(self):
+        result = part_one(self.example.splitlines())
+        # self.assertEqual(result, 18)
+
+    def test_part_two(self):
+        result = part_two(self.example.splitlines())
+        # self.assertEqual(result, xx)
+
+
+if __name__ == "__main__":
+    unittest.main()
