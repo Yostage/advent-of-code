975587fdfa73b1748ec4a267708cfbe3470f106b
Scott Yost <scott@yostage.com>
2025-01-03 10:00:21 -0800

    day10.1

--- 2024/day10.py
+++ 2024/day10.py
@@ -1,17 +1,44 @@
 import functools
 import re
+from collections import deque
 from dataclasses import dataclass, field
 from functools import cache
 from typing import Any, Deque, Dict, List, Set, Tuple, TypeVar
 
+from CharacterGrid import CharacterGrid, orthogonal_adjacencies
+from util import Point2D, tuple2_add
 
-def parse_lines(lines: List[str]) -> Any:
-    return None
+
+def parse_lines(lines: List[str]) -> CharacterGrid:
+    return CharacterGrid.from_lines(lines)
+
+
+def find_the_nines(start: Point2D, grid: CharacterGrid) -> int:
+    map = grid.map
+    visited = set()
+    to_visit = deque([start])
+    nines_found = 0
+    while to_visit:
+        loc = to_visit.popleft()
+        if loc in visited:
+            continue
+        visited.add(loc)
+        if map[loc] == "9":
+            nines_found += 1
+            continue
+        for direction in orthogonal_adjacencies:
+            new_loc = tuple2_add(loc, direction)
+            if new_loc in map:
+                if int(map[new_loc]) == int(map[loc]) + 1:
+                    to_visit.append(new_loc)
+    return nines_found
 
 
 def part_one(lines) -> int:
-    parse_lines(lines)
-    return 0
+    grid = parse_lines(lines)
+    # for every zero in the grid
+    # search how many nines are reachable, with single step increasing walk
+    return sum(find_the_nines(loc, grid) for loc, val in grid.map.items() if val == "0")
 
 
 def part_two(lines) -> int:
@@ -28,4 +55,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
