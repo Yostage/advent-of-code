cb745012cf103c6715a52d79ce8212b32edd4887
Scott Yost <scott@yostage.com>
2025-12-02 22:04:33 -0800

    2024.12 i guess

---
+++ 2024/day12_tests.py
@@ -0,0 +1,31 @@
+import unittest
+
+from day12 import parse_lines, part_one, part_two
+
+
+class TestDay12(unittest.TestCase):
+    example = """RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE"""
+
+    def test_parse_lines(self):
+        parse_lines(self.example.splitlines())
+
+    def test_part_one(self):
+        result = part_one(self.example.splitlines())
+        # self.assertEqual(result, 1930)
+
+    def test_part_two(self):
+        result = part_two(self.example.splitlines())
+        # self.assertEqual(result, xx)
+
+
+if __name__ == "__main__":
+    unittest.main()
