cb42e579af6c891cd3cc2d10e02e4a383dfc7dda
Scott Yost <scott@yostage.com>
2025-12-06 10:15:04 -0800

    day01.00

--- 2025/day01_tests.py
+++ 2025/day01_tests.py
@@ -4,14 +4,23 @@ from day01 import parse_lines, part_one, part_two
 
 
 class TestDay01(unittest.TestCase):
-    example = """"""
+    example = """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82"""
 
     def test_parse_lines(self):
         parse_lines(self.example.splitlines())
 
     def test_part_one(self):
         result = part_one(self.example.splitlines())
-        # self.assertEqual(result, xx)
+        self.assertEqual(result, 3)
 
     def test_part_two(self):
         result = part_two(self.example.splitlines())
@@ -20,4 +29,3 @@ class TestDay01(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
