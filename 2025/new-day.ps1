a8196a76561988be5566bc2d0b500e8198f7df51
Scott Yost <scott@yostage.com>
2025-12-21 10:39:44 -0800

    make new day do the amend too

--- 2025/new-day.ps1
+++ 2025/new-day.ps1
@@ -3,4 +3,5 @@ $day = "{0:D2}" -f [int]$day
 (get-content -path .\day_template._py -raw) -replace '9',$day | set-content .\day$day.py
 (get-content -path .\day_template_tests._py -raw) -replace '9',$day | set-content .\day${day}_tests.py
 new-item day${day}_input.txt -force | Out-Null
+hg commit -Am "day$day.00"
 # copy-item .\day_template.py .\day$day.py
