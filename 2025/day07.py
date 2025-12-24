f75a91a1cd24b8941b9afc6af428b74c344f2bc2
Scott Yost <scott@yostage.com>
2025-12-24 10:21:51 -0800

    day07.2

--- 2025/day07.py
+++ 2025/day07.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 from CharacterGrid import CharacterGrid
 from util import Point2D
 
@@ -8,7 +10,6 @@ def parse_lines(lines: list[str]) -> CharacterGrid:
 
 def part_one(lines) -> int:
     def split_laser(grid: CharacterGrid, pt: Point2D) -> None:
-        # left = Point2D(pt.x - 1, pt.y + 1)
         left = (pt[0] - 1, pt[1] + 1)
         right = (pt[0] + 1, pt[1] + 1)
         for pt in [left, right]:
@@ -21,7 +22,6 @@ def part_one(lines) -> int:
         for x in range(grid.width()):
             p = (x, y)
             if grid.map.get(p) == "S":
-                # print(f"S at ({x}, {y})")
                 grid.map[(p[0], p[1] + 1)] = "|"
             if grid.map.get(p) == "|":
                 dest = (p[0], p[1] + 1)
@@ -31,18 +31,40 @@ def part_one(lines) -> int:
                     total += 1
                     split_laser(grid, dest)
 
-                # print(f"S at ({x}, {y})")
-                # grid.map[(p[0], p[1] + 1)] = "|"
-
-            # if grid.map.get(p) == "^":
-
     grid.render()
     return total
 
 
+@lru_cache
+def recursive_splitter(grid: CharacterGrid, pt: Point2D) -> int:
+    def walk_down(grid: CharacterGrid, pt: Point2D) -> int:
+        while True:
+            if grid.map.get(pt) is None:
+                return 0
+            if grid.map.get(pt) == "^":
+                return recursive_splitter(grid, pt)
+            pt = (pt[0], pt[1] + 1)
+
+    left = (pt[0] - 1, pt[1] + 1)
+    right = (pt[0] + 1, pt[1] + 1)
+    return 1 + walk_down(grid, left) + walk_down(grid, right)
+
+
 def part_two(lines) -> int:
-    # grid = parse_lines(lines)
     total = 0
+    grid = parse_lines(lines)
+    for y in range(grid.height()):
+        row = grid.get_row_string(y)
+        x = row.find("S")
+        if x != -1:
+            break  # found the start point
+    # print(f"Start point is at ({x}, {y})")
+    while grid.map.get((x, y)) != "^":
+        y += 1
+    # print(f"First splitter is at ({x}, {y})")
+    return 1 + recursive_splitter(grid, (x, y))
+    # find the first splitter in the world
+
     return total
 
 
@@ -54,4 +76,5 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    main()
     main()
--- 2025/day07_tests.py
+++ 2025/day07_tests.py
@@ -30,7 +30,7 @@ class TestDay07(unittest.TestCase):
 
     def test_part_two(self):
         result = part_two(self.example.splitlines())
-        # self.assertEqual(result, xx)
+        self.assertEqual(result, 40)
 
 
 if __name__ == "__main__":
