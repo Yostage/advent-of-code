163266852e2f6f94cabe76396dde065362a5b47c
Scott Yost <scott@yostage.com>
2025-12-24 10:22:15 -0800

    day08.00

--- 2025/day08.py
+++ 2025/day08.py
@@ -21,7 +21,7 @@ def part_two(lines) -> int:
 
 
 def main() -> None:
-    with open("day9_input.txt", "r") as file:
+    with open("day08_input.txt", "r") as file:
         lines = file.read().splitlines()
         print(part_one(lines))
         print(part_two(lines))
@@ -29,3 +29,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
---
+++ 2025/day08_tests.py
@@ -0,0 +1,41 @@
+import unittest
+
+from day08 import parse_lines, part_one, part_two
+
+
+class TestDay08(unittest.TestCase):
+    example = """162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689"""
+
+    def test_parse_lines(self):
+        parse_lines(self.example.splitlines())
+
+    def test_part_one(self):
+        result = part_one(self.example.splitlines())
+        self.assertEqual(result, 40)
+
+    def test_part_two(self):
+        result = part_two(self.example.splitlines())
+        # self.assertEqual(result, xx)
+
+
+if __name__ == "__main__":
+    unittest.main()
