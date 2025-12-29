a4da02139dc8bef6fa3e0aab22743e6d39ef989a
Scott Yost <scott@yostage.com>
2025-12-28 16:25:51 -0800

    day10.00

---
+++ 2025/day10_tests.py
@@ -0,0 +1,24 @@
+import unittest
+
+from day10 import parse_lines, part_one, part_two
+
+
+class TestDay10(unittest.TestCase):
+    example = """[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"""
+
+    def test_parse_lines(self):
+        parse_lines(self.example.splitlines())
+
+    def test_part_one(self):
+        result = part_one(self.example.splitlines())
+        # self.assertEqual(result, xx)
+
+    def test_part_two(self):
+        result = part_two(self.example.splitlines())
+        # self.assertEqual(result, xx)
+
+
+if __name__ == "__main__":
+    unittest.main()
