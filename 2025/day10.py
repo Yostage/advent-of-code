9f517a843533d743d923d98eed54b997ce7b5969
Scott Yost <scott@yostage.com>
2025-12-30 13:10:40 -0800

    day10.02

--- 2025/day10.py
+++ 2025/day10.py
@@ -1,8 +1,12 @@
 import functools
 import re
+import time
 from dataclasses import dataclass, field
 from functools import cache
 from itertools import combinations
+
+import numpy as np
+from scipy.optimize import Bounds, LinearConstraint, milp
 
 
 def parse_lines(lines: list[str]):
@@ -18,7 +22,7 @@ def parse_lines(lines: list[str]):
         switch_toggles = [
             tuple(map(int, toggle[1:-1].split(","))) for toggle in switch_toggles_text
         ]
-        joltage_ratings = joltage_text[1:-1].split(",")
+        joltage_ratings = list(map(int, joltage_text[1:-1].split(",")))
         # print(output_lights, switch_toggles, joltage_ratings)
         switches.append((output_lights, switch_toggles, joltage_ratings))
 
@@ -38,8 +42,8 @@ def distributions(n, k):
         yield tuple(result)
 
 
-def min_presses(switch) -> int:
-    (output_lights, switch_toggles, joltage_ratings) = switch
+def min_presses_indicator_lights(switch) -> int:
+    (output_lights, switch_toggles, _) = switch
 
     for presses in range(len(switch_toggles) + 1):
         for distribution in distributions(presses, len(switch_toggles)):
@@ -52,47 +56,88 @@ def min_presses(switch) -> int:
                         lights_state[light_idx] = not lights_state[light_idx]
             if lights_state == output_lights:
                 return presses
-
-    # num_lights = len(output_lights)
-    # presses = 0
-    # for light_idx in range(num_lights):
-    #     if output_lights[light_idx]:
-    #         # Need this light ON
-    #         toggle_idxs = [
-    #             idx
-    #             for idx, toggle in enumerate(switch_toggles)
-    #             if light_idx in toggle
-    #         ]
-    #         if not toggle_idxs:
-    #             # No way to turn this light on
-    #             return float("inf")
-    #         # Choose the toggle with the lowest joltage rating
-    #         best_toggle_idx = min(
-    #             toggle_idxs,
-    #             key=lambda idx: int(joltage_ratings[idx]),
-    #         )
-    #         presses += int(joltage_ratings[best_toggle_idx])
-    return presses
+    raise RuntimeError(f"No solution found for output lights {output_lights}")
 
 
 def part_one(lines) -> int:
     switches = parse_lines(lines)
     # print(output_lights, switch_toggles, joltage_ratings)
 
-    return sum(min_presses(switch) for switch in switches)
+    return sum(min_presses_indicator_lights(switch) for switch in switches)
+
+
+def min_presses_joltage(switch) -> int:
+    (_, switch_toggles, joltage_ratings) = switch
+    start_time = time.time()
+    distributions_tested = 0
+    for presses in range(max(joltage_ratings), sum(joltage_ratings) + 1):
+        for distribution in distributions(presses, len(switch_toggles)):
+            distributions_tested += 1
+            if distributions_tested % 5000000 == 0:
+                current_time = time.time()
+                print(
+                    f" Tested {distributions_tested} distributions in {current_time - start_time:.2f} seconds"
+                )
+            # Apply this distribution of presses
+            joltage_state = [0] * len(joltage_ratings)
+            for toggle_idx, press_count in enumerate(distribution):
+                for light_idx in switch_toggles[toggle_idx]:
+                    joltage_state[light_idx] += press_count
+            if joltage_state == joltage_ratings:
+                print(
+                    f"Found solution with presses={presses} for joltage_ratings={joltage_ratings}"
+                )
+                endtime = time.time()
+                print(
+                    f" Tested {distributions_tested} distributions in {endtime - start_time:.2f} seconds"
+                )
+                print(
+                    f" Distributions per second: {distributions_tested / (endtime - start_time)}"
+                )
+                return presses
+            # else:
+            #     print(
+            #         f"  Tried presses={presses} distribution={distribution} got {joltage_state}"
+            #     )
+    raise RuntimeError(f"No solution found for joltage ratings {joltage_ratings}")
+
+
+def min_presses_joltage_scipy(switch) -> int:
+
+    (_, switch_toggles, joltage_ratings) = switch
+    switch_toggles_array = np.zeros(
+        (len(switch_toggles), len(joltage_ratings)), dtype=int
+    )
+    for toggle_idx, toggle in enumerate(switch_toggles):
+        for light_idx in toggle:
+            switch_toggles_array[toggle_idx, light_idx] = 1
+
+    A = switch_toggles_array
+    B = np.array(joltage_ratings)
+    n_vars = A.shape[0]  # 6 items
+
+    result = milp(
+        c=np.ones(n_vars),  # minimize sum(X)
+        constraints=LinearConstraint(A.T, B, B),
+        integrality=np.ones(n_vars),  # all integers
+        bounds=Bounds(lb=0, ub=max(joltage_ratings)),
+    )
+    assert result.success
+    assert result.status == 0
+    # print(f"{result.x} : sum:{int(sum(result.x))}, fun:{result.fun}")
+    return round(result.fun)
 
 
 def part_two(lines) -> int:
-    parse_lines(lines)
-    total = 0
-    return total
+    switches = parse_lines(lines)
+    return sum(min_presses_joltage_scipy(switch) for switch in switches)
 
 
 def main() -> None:
     with open("day10_input.txt", "r") as file:
         lines = file.read().splitlines()
-        print(part_one(lines))
-        print(part_two(lines))
+    print(part_one(lines))
+    print(part_two(lines))
 
 
 if __name__ == "__main__":
--- 2025/day10_tests.py
+++ 2025/day10_tests.py
@@ -8,6 +8,8 @@ class TestDay10(unittest.TestCase):
 [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"""
 
+    rong = """[##.##.###] (0,1,4,5,6,7,8) (1,2,3,4,5,6,8) (1,4,6) (1,5,6) (0,2,4,5,7) (0,1,3,4,6,7,8) (0,2,6,8) (2,3,6,7) (0,2,3,4,6,7) (2,5) (0,1,3,4,5,6,7,8) {87,72,71,70,99,64,112,89,62}"""
+
     def test_parse_lines(self):
         parse_lines(self.example.splitlines())
 
@@ -17,7 +19,11 @@ class TestDay10(unittest.TestCase):
 
     def test_part_two(self):
         result = part_two(self.example.splitlines())
-        # self.assertEqual(result, xx)
+        self.assertEqual(result, 33)
+
+    def test_part_two_rong(self):
+        result = part_two(self.rong.splitlines())
+        self.assertEqual(result, 128)
 
 
 if __name__ == "__main__":
