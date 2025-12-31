5132b2437a356310e2078ab9f9f107952fe23984
Scott Yost <scott@yostage.com>
2025-12-30 19:41:32 -0800

    day11.01

--- 2025/day11.py
+++ 2025/day11.py
@@ -1,16 +1,43 @@
 import functools
 import re
+from collections import defaultdict, deque
 from dataclasses import dataclass, field
 from functools import cache
 
 
-def parse_lines(lines: list[str]) -> any:
-    return None
+def parse_lines(lines: list[str]) -> list[tuple[str, list[str]]]:
+    out = []
+    for line in lines:
+        (left, right) = line.split(": ")
+        out.append((left, right.split()))
+
+    return out
 
 
 def part_one(lines) -> int:
-    parse_lines(lines)
+    graph = parse_lines(lines)
+    edges = defaultdict(set)
+    for src, dsts in graph:
+        for dst in dsts:
+            edges[src].add(dst)
     total = 0
+    path = ("you",)
+    visited = set()
+    stack = deque()
+    stack.appendleft((path, visited))
+    while stack:
+        (path, visited) = stack.pop()
+        current = path[-1]
+        visited.add(current)
+
+        for neighbor in edges[current]:
+            if neighbor in visited:
+                continue
+            stack.appendleft((path + (neighbor,), visited.copy()))
+            if neighbor == "out":
+                total += 1
+                print(f"Path {total}: {path}")
+
     return total
 
 
@@ -29,4 +56,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
--- 2025/day11_tests.py
+++ 2025/day11_tests.py
@@ -4,14 +4,23 @@ from day11 import parse_lines, part_one, part_two
 
 
 class TestDay11(unittest.TestCase):
-    example = """"""
+    example = """aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out"""
 
     def test_parse_lines(self):
         parse_lines(self.example.splitlines())
 
     def test_part_one(self):
         result = part_one(self.example.splitlines())
-        # self.assertEqual(result, xx)
+        self.assertEqual(result, 5)
 
     def test_part_two(self):
         result = part_two(self.example.splitlines())
@@ -20,4 +29,3 @@ class TestDay11(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
