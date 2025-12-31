bd643f98886875d53a689feaea534f3b63a4ec03
Scott Yost <scott@yostage.com>
2025-12-31 15:52:15 -0800

    day12.01

--- 2025/day12.py
+++ 2025/day12.py
@@ -3,13 +3,38 @@ import re
 from dataclasses import dataclass, field
 from functools import cache
 
+from CharacterGrid import CharacterGrid
 
-def parse_lines(lines: list[str]) -> any:
-    return None
+
+def parse_lines(lines: list[str]):
+    def eat_grid(iter):
+        new_grid = []
+        for line in line_iter:
+            if line == "":
+                return CharacterGrid.from_lines(new_grid)
+            else:
+                new_grid.append(line)
+
+    line_iter = iter(lines)
+    grids = []
+    regions = []
+    for line in line_iter:
+        if line[1] == ":":
+            grids.append(eat_grid(line_iter))
+        if "x" in line:
+            (left, right) = line.split(":")
+            width, height = left.split("x")
+            present_requirements = tuple(map(int, right.split()))
+            regions.append(((int(width), int(height)), present_requirements))
+
+    return (grids, regions)
 
 
 def part_one(lines) -> int:
-    parse_lines(lines)
+    (grids, regions) = parse_lines(lines)
+    for grid in grids:
+        grid.render()
+    print(regions)
     total = 0
     return total
 
@@ -23,10 +48,10 @@ def part_two(lines) -> int:
 def main() -> None:
     with open("day12_input.txt", "r") as file:
         lines = file.read().splitlines()
+        # not zero
         print(part_one(lines))
         print(part_two(lines))
 
 
 if __name__ == "__main__":
     main()
-
--- 2025/day12_tests.py
+++ 2025/day12_tests.py
@@ -4,14 +4,46 @@ from day12 import parse_lines, part_one, part_two
 
 
 class TestDay12(unittest.TestCase):
-    example = """"""
+    example = """0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2"""
 
     def test_parse_lines(self):
         parse_lines(self.example.splitlines())
 
     def test_part_one(self):
         result = part_one(self.example.splitlines())
-        # self.assertEqual(result, xx)
+        self.assertEqual(result, 2)
 
     def test_part_two(self):
         result = part_two(self.example.splitlines())
@@ -20,4 +52,3 @@ class TestDay12(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
